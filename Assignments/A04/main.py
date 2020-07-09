9ee39ddfdce76cbc2ed709119000c2bcb2051390
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-07-09 18:03:43 -0500

    Update main.py

--- Assignments/A04/main.py
+++ Assignments/A04/main.py
@@ -1,9 +1,1 @@
-{
-    "fname":"Sasha",
-    "lname":"Ivanov",
-    "rank": 15339,
-    "screen_name":"spetsnaz983",
-    "email":"sashaivanov1998@gru.gov",
-    "power-boost":98.34,
-    "available-boosts": ["skull-crush","flower-child","acid-trip","disco-ball"]
-}
+
